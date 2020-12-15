f690f6f18a6c75db5e05c43db6eb61c2ca11fc62
Jimoh Abdul <jimohabdulquadrik@gamil.com>
2020-12-15 07:43:55 +0100

    'Quiz'

---
+++ questions.js
@@ -0,0 +1,80 @@
+let questions = [{
+        numb: 1,
+        question: "How many valves does the heart have??",
+        answer: "Four",
+        options: [
+            "Six",
+            "Eight",
+            "Four",
+            "Five"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the largest country in the world?",
+        answer: "Russia",
+        options: [
+            "America",
+            "Russia",
+            "Japan",
+            "Nigeria"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which fashion designer reportedly had a relationship with Winston Churchill?",
+        answer: "Coco Chanel",
+        options: [
+            "Fenty",
+            "Ashluxe",
+            "Coco Chanel",
+            "Christian Louboutin"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What's the biggest animal in the world?",
+        answer: "Whale",
+        options: [
+            "Dinosaur",
+            "Elephant",
+            "Sea Lion",
+            "Whale"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How many minutes in a game of rugby league?",
+        answer: "80 minutes",
+        options: [
+            "90 minutes",
+            "80 minutes",
+            "75 minutes",
+            "95 minutes"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "What fruit takes the scientific name Mangifera indica?",
+        answer: "Mango",
+        options: [
+            "Mango",
+            "Banana",
+            "Cashew",
+            "Orange"
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "In what part of the body would you find the fibula?",
+        answer: "Leg",
+        options: [
+            "Arm",
+            "Waist",
+            "Neck",
+            "Leg"
+        ]
+    },
+];
